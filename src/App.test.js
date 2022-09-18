62a0a3ec543bac468b0de567854e52c7112a6e05
Benjamin <60583572+benmarincanseco@users.noreply.github.com>
2022-09-17 23:42:56 -0400

    New test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('render text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/This starter project enables you to build web application using React Native module./i);
+  expect(linkElement).toBeInTheDocument();
+});
